437c861688dd1aeca5dc770f83af71b61b58ed5d
Abanoub Ghadban <abanoub.gdb@gmail.com>
2020-03-31 07:35:23 +0200

    hotfix: replaced shift left operator

--- p0.1.0/project0/config.h
+++ p0.1.0/project0/config.h
@@ -3,9 +3,9 @@
 
 #include "tm4c123gh6pm.h"
 
-#define PEDSTARIAN_SW				(*(GPIO_PORTF_DATA_BITS_R + ((1U << 0) < 1)))
-#define RTL_TRAIN_SW				(*(GPIO_PORTF_DATA_BITS_R + ((1U << 4) < 1)))
-#define LTR_TRAIN_SW				(*(GPIO_PORTE_DATA_BITS_R + ((1U << 4) < 1)))
+#define PEDSTARIAN_SW				(*(GPIO_PORTF_DATA_BITS_R + ((1U << 0) << 1)))
+#define RTL_TRAIN_SW				(*(GPIO_PORTF_DATA_BITS_R + ((1U << 4) << 1)))
+#define LTR_TRAIN_SW				(*(GPIO_PORTE_DATA_BITS_R + ((1U << 4) << 1)))
 
 #define TRAFFIC_LIGHT				(*(GPIO_PORTE_DATA_BITS_R + (0xF < 1)))
 #define TRAFFIC_RED_NS			(1U << 0)
@@ -14,7 +14,7 @@
 #define TRAFFIC_GREEN_EW		(1U << 3)
 #define TRAFFIC_OFF					(0x0)
 
-#define BOARD_LED						(*(GPIO_PORTF_DATA_BITS_R + (0xE < 3)))
+#define BOARD_LED						(*(GPIO_PORTF_DATA_BITS_R + (0xE << 3)))
 #define BOARD_LED_RED				(1U << 1)
 #define BOARD_LED_BLUE			(1U << 2)
 #define BOARD_LED_GREEN			(1U << 3)
